a468e4de8858b82e3551f22995942f1af4c07efd
grupodvbr <grupodvbr@gmail.com>
2026-03-05 11:04:14 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install",e=>{
+self.skipWaiting()
+})
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+fetch(e.request).catch(()=>{
+return new Response("Offline")
+})
+)
+})
